23590c87cda763da98f614551cdbded2f11a7256
Aluno <aluno@estacio.acad>
2024-10-28 21:23:33 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function openmenuhamburguer() {
+  const menu = document.querySelector(".menuhamburguer");
+  menu.classList.add("menuopen");
+}
+function closemenuhamburguer() {
+  const menu = document.querySelector(".menuhamburguer");
+  menu.classList.remove("menuopen");
+}
