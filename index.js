247e95b5241f4eee3bf29e0bb17cec5d69fcc0c9
caioarlan18 <caioarlan58@gmail.com>
2024-11-06 00:10:26 -0300

    frontend pronto

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function closemenuhamburguer() {
   const menu = document.querySelector(".menuhamburguer");
   menu.classList.remove("menuopen");
 }
+function search() {
+  const pesquisa = prompt("Pesquisa");
+  if (pesquisa) {
+    alert(`Pesquisou: ${pesquisa}`)
+  }
+
+}
