da3ff1f199aef85211cdde72cfd1bd37ed7ed2ad
Caio Arlan Gomes <108774432+caioarlan18@users.noreply.github.com>
2024-11-11 19:01:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,33 +14,14 @@ function search() {
 
 }
 
+
 function email(nome, email) {
-  const assunto = "Promoções Imperdíveis do Elizangela Schinaider Studio!";
-
-  // Corpo do email com formatação (em texto simples, já que o mailto não suporta HTML)
-  const corpo = `
-    Olá ${nome},\n\n
-    Esperamos que você esteja bem! Aqui estão algumas promoções imperdíveis para você aproveitar no nosso Studio de Beleza:
-
-    -------------------------------------------------------------
-    SERVIÇOS EXCLUSIVOS PARA VOCÊ:
-    -------------------------------------------------------------
-    1. Manicure e Pedicure: 20% OFF - **R$ 50,00**
-    2. Depilação a Laser: 10% OFF - **R$ 150,00**
-    3. Design de Sobrancelhas: **Grátis** na Primeira Sessão - **R$ 80,00**
-    4. Tratamento Capilar: 10% OFF - **R$ 120,00**
-    
-    -------------------------------------------------------------
-    **Agende seu horário conosco e aproveite essas promoções especiais!**
-    
-    Atenciosamente,
-    Elizangela Schinaider Studio
-    -------------------------------------------------------------
-    www.elizangelastudio.com.br
-  `;
-
-  const gmailUrl = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${encodeURIComponent(assunto)}&body=${encodeURIComponent(corpo)}`;
-
-  window.open(gmailUrl, '_blank');
+  const params = {
+    name: nome,
+    email: email
+  };
+  emailjs.send("service_3l5cogi", "template_ar0v5zo", params).then(() => {
+    alert('Email enviado com sucesso');
+  });
 }
 
